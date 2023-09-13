4fce9c902b98a854d9e180c1fe9ead9ed2b913cf
wzj <826276672@qq.com>
2023-09-13 16:36:01 +0800

    测试一下

---
+++ app/src/main/java/com/wzj/test/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wzj.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ mylibrary/src/main/java/com/wzj/mylibrary/Test.kt
@@ -0,0 +1,7 @@
+package com.wzj.mylibrary
+
+class Test {
+    fun plus(a: Int, b: Int): Int {
+        return a + b
+    }
+}
